3d5354e6d958a6a611f4fddf10e9cd4fee7ee126
Alex Zheng <alexander_zheng@brown.edu>
2021-11-30 16:39:44 -0500

    af

--- servo_tester.py
+++ servo_tester.py
@@ -1,8 +1,17 @@
-from gpiozero import Servo
+from gpiozero import Servo, AngularServo
 from time import sleep
 
-servo = Servo(17)
+servo = AngularServo(14)
 
-while True:
-       servo.mid()
-       sleep(0.5)
+
+def unlock():
+     servo.angle = 90
+
+def lock():
+     servo.angle = 0
+
+
+if __name__ == "__main__":
+    #take in user signal
+    #call lock/unlock
+
