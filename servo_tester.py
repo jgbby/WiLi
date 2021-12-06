d6f2de51a72329b0d217cb36a514a1e9d2529f63
Alex Zheng <alexander_zheng@brown.edu>
2021-12-06 13:31:43 -0500

    sdaf

--- servo_tester.py
+++ servo_tester.py
@@ -4,14 +4,28 @@ from time import sleep
 servo = AngularServo(14)
 
 
-def unlock():
-     servo.angle = 90
+def unlock(angle):
+     '''
+     angle - the angle to set the servo at UNLOCK position
+     '''
+     servo.angle = angle
 
-def lock():
-     servo.angle = 0
+def lock(angle):
+     '''
+     angle - the angle to set the servo at LOCK position
+     '''
+     servo.angle = angle
+
+def timer_lock(lock_angle, unlock_angle, time):
+     '''
+     time - the time that you want to keep the lock locked for
+     '''
+     #the 0 and 90 in the lock and unlock methods can be changed
+     lock(lock_angle)
+     sleep(time)
+     unlock(unlock_angle)
 
 
 if __name__ == "__main__":
-    #take in user signal
-    #call lock/unlock
+    
 
